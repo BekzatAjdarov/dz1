ee511794a6032777db269b3116c37d58c0e4fc89
Aidarbek uulu Bekzat <ajdarovbeka137@gmail.com>
2023-12-25 20:17:00 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Ведите ваше имя  ");
+        String username = scan.nextLine();
+        System.out.println("Привет "+username);
+        String text = "";
+        System.out.print("Ведите число от -1 до 100 ");
+        int num = scan.nextInt();
+        System.out.println(num+ " " +text);
+        if(num<0) {
+            System.out.println("Вы сохранили отрицательное число");
+        }
+        if(num>0) {
+            System.out.println("Вы сохранили положительное число");
+        }else {
+            System.out.println("иначе вы сохранили ноль");
+        }
+    }
+}
